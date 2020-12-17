cb895428c561f430cfd92041b2a0a9cdd6521567
Marco Leichsenring <MarcoL@web.de>
2020-12-17 11:08:19 +0100

    Complete Cloud Stream Kafka Stream showcase

--- src/main/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaStreams.java
+++ src/main/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaStreams.java
@@ -1,6 +1,7 @@
 package de.mle.stackoverflow.kafka;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
@@ -16,7 +17,7 @@ import java.util.function.Function;
 @Configuration
 public class CloudStreamWithKafkaStreams {
     @Bean
-    public Function<KStream<Object, String>, KStream<?, WordCount>> process() {
+    public Function<KStream<Bytes, String>, KStream<Bytes, WordCount>> process() {
         return input -> input
                 .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
                 .map((key, value) -> new KeyValue<>(value, value))
@@ -24,6 +25,6 @@ public class CloudStreamWithKafkaStreams {
                 .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
                 .count(Materialized.as("word-counts-state-store"))
                 .toStream()
-                .map((key, value) -> new KeyValue<>(key.key(), new WordCount(key.key(), value)));
+                .map((key, value) -> new KeyValue<>(null, new WordCount(key.key(), value)));
     }
 }
--- src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaStreamsIT.java
+++ src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaStreamsIT.java
@@ -5,30 +5,45 @@ import de.mle.stackoverflow.IntegrationTestConfigWithPortAndTestProfile;
 import lombok.SneakyThrows;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CloudStreamWithKafkaStreamsIT extends IntegrationTestConfigWithPortAndTestProfile {
     @Test
-    @Disabled
+    @SneakyThrows
     void process() {
         // given
         initTestQueueReceiverForTopic("counts");
 
         // when
-        sendMessage("Wort Buch Wort", "first-words", "words");
+        sendMessage("Wort Buch Wort", null, "words");
+        sendMessage("Auto Auto Wort", null, "words");
+        Thread.sleep(6_000);
+        sendMessage("Auto Affe", null, "words");
 
         // then
         Awaitility.await().untilAsserted(() ->
                 assertThat(records.stream()
                         .map(ConsumerRecord::value)
                         .filter(Objects::nonNull)
-                        .map(this::parseValue))
-                        .contains(new WordCount("Buch", 1l), new WordCount("Wort", 2l)));
+                        .map(this::parseValue)
+                        .collect(Collectors.toList())
+                )
+                        .containsExactlyInAnyOrder(
+                                // from first time window
+                                new WordCount("", 2l),
+                                new WordCount("buch", 1l),
+                                new WordCount("auto", 2l),
+                                new WordCount("wort", 3l),
+                                // from second time window
+                                new WordCount("", 1l),
+                                new WordCount("auto", 1l),
+                                new WordCount("affe", 1l)
+                                ));
     }
 
     @SneakyThrows
