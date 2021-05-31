aabc7856582acffe3d4689b21637f49bfe01ca84
Marco Leichsenring <MarcoL@web.de>
2021-05-31 12:55:50 +0200

    Separate Spring Cloud Kafka Streams tests

--- src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaEndToEndIT.java
+++ src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaEndToEndIT.java
@@ -7,7 +7,6 @@ import java.util.Objects;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.DirtiesContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -15,7 +14,6 @@ import de.mle.stackoverflow.IntegrationTestConfigWithPortAndTestProfile;
 
 import lombok.SneakyThrows;
 
-@DirtiesContext
 public class CloudStreamWithKafkaEndToEndIT extends IntegrationTestConfigWithPortAndTestProfile {
     @Test
     void producerProcessorConsumer() {
--- src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaLowLevelIT.java
+++ src/test/java/de/mle/stackoverflow/kafka/CloudStreamWithKafkaLowLevelIT.java
@@ -3,41 +3,37 @@ package de.mle.stackoverflow.kafka;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.InputDestination;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.Message;
 import org.springframework.test.annotation.DirtiesContext;
 
-import de.mle.stackoverflow.StackoverflowApplication;
+import de.mle.stackoverflow.IntegrationTestConfigWithPortAndTestProfile;
 
 @DirtiesContext
-public class CloudStreamWithKafkaLowLevelIT {
+@Import(TestChannelBinderConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = "spring.cloud.stream.function.definition=concatHash;concatStar")
+public class CloudStreamWithKafkaLowLevelIT extends IntegrationTestConfigWithPortAndTestProfile {
+    @Autowired
+    private InputDestination inputDestination;
+    @Autowired
+    private OutputDestination outputDestination;
+
     @Test
     public void testMultipleFunctions() {
-        try (ConfigurableApplicationContext context = new SpringApplicationBuilder(
-                TestChannelBinderConfiguration.getCompleteConfiguration(
-                        StackoverflowApplication.class))
-                .web(WebApplicationType.SERVLET)
-                .run("--spring.cloud.stream.function.definition=concatHash;concatStar")) {
-            context.getBean(InputDestination.class);
+        Message<byte[]> inputMessage = MessageBuilder.withPayload("Hello".getBytes()).build();
+        inputDestination.send(inputMessage, "concatHash-in-0");
+        inputDestination.send(inputMessage, "concatStar-in-0");
 
-            InputDestination inputDestination = context.getBean(InputDestination.class);
-            OutputDestination outputDestination = context.getBean(OutputDestination.class);
+        Message<byte[]> outputMessage = outputDestination.receive(0, "concatHash-out-0");
+        assertThat(outputMessage.getPayload()).isEqualTo("Hello#".getBytes());
 
-            Message<byte[]> inputMessage = MessageBuilder.withPayload("Hello".getBytes()).build();
-            inputDestination.send(inputMessage, "concatHash-in-0");
-            inputDestination.send(inputMessage, "concatStar-in-0");
-
-            Message<byte[]> outputMessage = outputDestination.receive(0, "concatHash-out-0");
-            assertThat(outputMessage.getPayload()).isEqualTo("Hello#".getBytes());
-
-            outputMessage = outputDestination.receive(0, "concatStar-out-0");
-            assertThat(outputMessage.getPayload()).isEqualTo("Hello*".getBytes());
-        }
+        outputMessage = outputDestination.receive(0, "concatStar-out-0");
+        assertThat(outputMessage.getPayload()).isEqualTo("Hello*".getBytes());
     }
 }
